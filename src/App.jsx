c808ee98b8c5e534de781ba10bfe48b34da824d9
aliadde <aliadely1384@gmail.com>
2024-09-19 14:49:18 +0330

    box , adad ha , jaigah ha , html , css kamel shod

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useContext } from "react";
+import { calculaterContext } from "./context.js";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="text-5xl text-indigo-500">Hi</div>
+      <calculaterContext.Provider>
+        <div id="container" className="bg-indigo-300">
+          <div className="showresualt"> </div>
+          <div className="mohasebat">
+            <ul>
+              <ul className="setaaval">
+                <li className="adad1">1</li>
+                <li className="adad2">2</li>
+                <li className="adad3">3</li>
+              </ul>
+
+              <ul className="setadovom">
+                <li className="adad4">4</li>
+                <li className="adad5">5</li>
+                <li className="adad6">6</li>
+              </ul>
+              <ul className="setasevom">
+                <li className="adad7">7</li>
+                <li className="adad8">8</li>
+                <li className="adad9">9</li>
+              </ul>
+
+              {/* amaliyat ha */}
+              <ul className="side">
+                <li className="taghsim">
+                  <i className="fas fa-divide"></i>
+                </li>
+                <li className="zarb">
+                  <i className="fas fa-times"></i>
+                </li>
+                <li className="jam">
+                  <i className="fas fa-plus"></i>
+                </li>
+                <li className="tafrigh">
+                  <i className="fas fa-minus"></i>
+                </li>
+                <li className="mosavi">
+                  <i class="fas fa-equals"></i>
+                </li>
+              </ul>
+            </ul>
+          </div>
+        </div>
+      </calculaterContext.Provider>
     </>
   );
 }
